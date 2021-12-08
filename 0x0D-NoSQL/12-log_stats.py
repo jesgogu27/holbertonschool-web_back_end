732a26e1e21e239312912f4a6ab615123926ec53
jesgogu27 <jesgogu@hotmail.com>
2021-12-07 20:45:31 -0500

    NoSQL

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Log stats"""
+from pymongo import MongoClient
+
+
+def log(a: dict) -> int:
+    """return a log"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_logs = client.logs.nginx
+    return nginx_logs.count_documents(a)
+
+
+def logs_stats():
+    """provides some stats about Nginx logs stored in MongoDB"""
+    print(f"{ log({}) } logs")
+    print("Methods:")
+    print(f"\tmethod GET: { log({'method': 'GET'}) }")
+    print(f"\tmethod POST: { log({'method': 'POST'}) }")
+    print(f"\tmethod PUT: {log({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {log({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {log({'method': 'DELETE'})}")
+    print(f"{log({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    logs_stats()
